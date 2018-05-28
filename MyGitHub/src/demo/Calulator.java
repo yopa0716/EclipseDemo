255e1103f9fa359d6e0f4985ae46278368c758c7
yopa0716 <yopa0716@WSMPN-019.netcracker.com>
2018-05-28 18:00:59 +0530

    first commit Uploading file

---
+++ MyGitHub/src/demo/Calulator.java
@@ -0,0 +1,15 @@
+package demo;
+
+public class Calulator {
+
+	public int add(int num1,int num2){
+		int r= num1 + num2;
+		return r;
+	}
+	public int sub(int num1,int num2){
+		int r= num1 - num2;
+		return r;
+		
+	}
+	
+}
---
+++ MyGitHub/src/demo/exec.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class exec {
+
+	public static void main(String[] args) {
+	
+	}
+
+}
